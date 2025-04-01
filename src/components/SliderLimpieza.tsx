58b8990bf2d986e8b82683de174cba1e372c0c60
David <david-a.m.p.9@hotmail.com>
2025-04-01 10:51:18 -0500

    Se arreglo el slider

--- src/components/SliderLimpieza.tsx
+++ src/components/SliderLimpieza.tsx
@@ -1,45 +1,45 @@
+import { useRef } from "react";
 import Card from "./Card";
 
 const SliderLimpieza = () => {
-return (
-    <div className="contenedor_limpieza">
-    <button className="contenedor_icon_button ">
-        <img src="src/assets/icons/arrow.png" alt="Flecha izquierda"  className="pre-btn" />
-    </button>
+    const sliderRef = useRef<HTMLDivElement>(null); 
 
-    <Card 
-        nombre="Kit de Limpieza" 
-        precio={15000} 
-        url={"src/assets/img/limpieza/kitLimpieza.jpg"}  
-        descripcion="Kit de limpieza interno y externo" 
-    /> 
+    const scrollLeft = () => {
+        if (sliderRef.current) {
+            sliderRef.current.scrollLeft -= 250; 
+        }
+    };
 
-<Card 
-        nombre="Kit de Limpieza" 
-        precio={15000} 
-        url={"src/assets/img/limpieza/kitLimpieza.jpg"}  
-        descripcion="Kit de limpieza interno y externo" 
-    /> 
+    const scrollRight = () => {
+        if (sliderRef.current) {
+            sliderRef.current.scrollLeft += 250; 
+        }
+    };
 
-<Card 
-        nombre="Kit de Limpieza" 
-        precio={15000} 
-        url={"src/assets/img/limpieza/kitLimpieza.jpg"}  
-        descripcion="Kit de limpieza interno y externo" 
-    /> 
+    return (
+        <div className="contenedor_limpieza">
+            <button className="contenedor_icon_button" onClick={scrollLeft}>
+                <img src="src/assets/icons/arrow.png" alt="Flecha izquierda" className="pre-btn" />
+            </button>
 
-<Card 
-        nombre="Kit de Limpieza" 
-        precio={15000} 
-        url={"src/assets/img/limpieza/kitLimpieza.jpg"}  
-        descripcion="Kit de limpieza interno y externo" 
-    /> 
+            {/* Agregamos la ref directamente al slider */}
+            <div className="slider" ref={sliderRef}>
+                {[...Array(10)].map((_, index) => (
+                    <Card
+                        key={index}
+                        nombre="Kit de Limpieza"
+                        precio={15000}
+                        url="src/assets/img/limpieza/kitLimpieza.jpg"
+                        descripcion="Kit de limpieza interno y externo"
+                    />
+                ))}
+            </div>
 
-    <button className="contenedor_icon_button">
-        <img src="src/assets/icons/arrow.png" alt="Flecha derecha" className="nxt-btn"/>
-    </button>
-    </div>
-);
+            <button className="contenedor_icon_button" onClick={scrollRight}>
+                <img src="src/assets/icons/arrow.png" alt="Flecha derecha" className="nxt-btn" />
+            </button>
+        </div>
+    );
 };
 
 export default SliderLimpieza;
